7a43d52b28b9605cafbb714fb63c7e994acbe130
TsitoUw <tsitohainanirinarak2@gmail.com>
2023-03-29 00:09:13 +0300

    Edit: added copyright and adjust contrast, some animations too

--- src/views/home/home_view.js
+++ src/views/home/home_view.js
@@ -5,12 +5,17 @@ export default () => createComponent(() => {
 
   return e("div", { id: "home" }, [
     e('div',{class:'emoji'}, '🍞️'), 
-    e('h2',{class:'title'},'Hello world!'),
+    e('h2',{class:'title | glitch'},'Hello world!'),
     e('p',{class:'thanking'},[
       "thanks for downloading this, don't forget to put a star on it on ",
       e('a',{href:'https://github.com/TsitoUw/vanilla-spa-boilerplate',target:'_blank'},"github")
     ]),
-    e('button',{id:'count', ariaLabel:'increment count'},'count: 0')
+    e('button',{id:'count', ariaLabel:'increment count'},'count: 0'),
+    e('p',{id:'copyright'}, [
+      'Made with ❤️ by ',
+      e('a',{href:'https://github.com/TsitoUw',target:'_blank'},"Tsito")
+
+    ]) 
   ]);
 }, "src/views/home/home_script.js", "src/views/home/home_style.css");
  
